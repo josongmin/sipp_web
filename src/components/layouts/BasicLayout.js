260e92a0d7579f96bafbb2ffc93e7003d69af390
jaewon <chocholand@naver.com>
2019-01-04 02:34:28 +0900

    [dev: layout] - change to topnav

--- src/components/layouts/BasicLayout.js
+++ src/components/layouts/BasicLayout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import LeftSidebar from './LeftSidebar'
+import HorizontalNav from './HorizontalNav'
 import TopBar from './TopBar'
 import Protected from '../decorator/Protected'
 
@@ -10,11 +11,12 @@ export default class BasicLayout extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <LeftSidebar {...this.props} />
         <div className="content-page">
           <div className="content">
-            <TopBar />
-            {this.props.children}
+            <HorizontalNav {...this.props} />
+            <div className="container-fluid">
+              {this.props.children}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/layouts/HorizontalNav.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react'
+import { observer } from 'mobx-react'
+import { Link } from 'react-router-dom'
+
+@observer
+export default class HorizontalNav extends React.Component {
+  render() {
+    const { match } = this.props
+    return (
+      <Fragment>
+        <div className="navbar-custom topnav-navbar">
+          <div className="container-fluid">
+            <Link to={'/'} className="logo">
+              <h1 className="logo-lg" style={{ margin: 0, paddingTop: 15 }}>
+                SHOPEE
+              </h1>
+            </Link>
+            <a className="navbar-toggle" data-toggle="collapse" data-target="#topnav-menu-content">
+              <div className="lines">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="topnav">
+          <div className="container-fluid">
+            <nav className="navbar navbar-dark navbar-expand-lg topnav-menu">
+
+              <div className="collapse navbar-collapse" id="topnav-menu-content">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link to="/" className={`nav-link ${match.path === '/' ? 'active' : ''}`}>
+                      <i className="mdi mdi-speedometer mr-1"/>
+                      <span> DASHBOARD </span>
+                    </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link to={'/transaction'} className={`nav-link ${match.path === '/transaction' ? 'active' : ''}`}>
+                      <i className="mdi mdi-google-pages mr-1"/>
+                      <span> 거래조회 </span>
+                    </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link to={'/notice'} className={`nav-link ${match.path === '/notice' ? 'active' : ''}`}>
+                      <i className="mdi mdi-briefcase-outline mr-1"/>
+                      <span> 공지사항 </span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
+}
