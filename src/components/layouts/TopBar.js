9c9b02e9a2f2e2e6f5a51bae8387163365e39256
jaewon <chocholand@naver.com>
2019-01-03 18:31:27 +0900

    [dev: topbar] - 검색 버튼 삭제

--- src/components/layouts/TopBar.js
+++ src/components/layouts/TopBar.js
@@ -10,15 +10,7 @@ export default class TopBar extends React.Component {
           <i className="mdi mdi-menu" />
         </button>
         <div className="app-search">
-          <form>
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="주문번호 입력" />
-                <span className="mdi mdi-magnify" />
-                <div className="input-group-append">
-                  <button className="btn btn-primary" type="submit">Search</button>
-                </div>
-            </div>
-          </form>
+          
         </div>
       </div>
     )
