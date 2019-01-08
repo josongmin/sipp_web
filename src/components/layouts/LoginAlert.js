65b59db0901e5fee1c90545d0208bf44209b7903
jaewon <chocholand@naver.com>
2019-01-08 19:05:44 +0900

    [style: login] - alert popup 에 OK버튼 추가

--- src/components/layouts/LoginAlert.js
+++ src/components/layouts/LoginAlert.js
@@ -8,13 +8,14 @@ export default class LoginAlert extends React.Component {
   }
 
   render() {
-    const { msg } = this.props
+    const { msg, onToggleModal } = this.props
     return (
       <div className="modal-filled bg-danger" style={{ padding: '20px 40px' }}>
         <div className="text-center">
           <i className="dripicons-wrong h1" />
           <h4 className="mt-2">Oh snap!</h4>
           <p className="mt-3"> {msg}</p>
+          <button type="button" className="btn btn-light my-2" data-dismiss="modal" onClick={onToggleModal}>OK!</button>
         </div>
       </div>
     )
