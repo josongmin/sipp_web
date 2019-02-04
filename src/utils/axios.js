5e99a92a23560983bf9c7006f1cd1f81fcf95d74
josongmin <944899@gmail.com>
2019-02-04 12:43:09 +0900

    a

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios"
 import qs from "qs"
 import { stores } from '../stores'
 
-export const URL = "http://localhost:10008/v1/admin"
-export const LINK_URL = "http://54.169.44.55"
+export const URL = "http://http://124.158.132.30:10008/v1/admin"
+export const LINK_URL = "http://http://124.158.132.30"
 
 // export const URL = "http://localhost:2112";
 // export const LINK_URL = "http://localhost:3000";
